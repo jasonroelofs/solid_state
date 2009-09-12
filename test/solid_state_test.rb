f6cc6446e095543444fccf38d0513e9446663c43
Jason Roelofs <jameskilton@gmail.com>
2009-09-12 14:51:13 -0400

    Updated API from discussions, should be more internally consistent and easier to understand

--- test/solid_state_test.rb
+++ test/solid_state_test.rb
@@ -52,6 +52,20 @@ class Stateful
   end
 end 
 
+class SubState < Stateful
+
+  state :another do
+
+    def add(a, b)
+      a % b
+    end
+
+  end
+
+  starting_state :another
+
+end
+
 context "SolidState" do
 
   before do
@@ -61,33 +75,27 @@ context "SolidState" do
   context "Query and changing state" do
 
     specify "can get current state" do
-      @stateful.state.should.be nil
-      @stateful.state = :start
-      @stateful.state.should.equal :start
+      @stateful.current_state.should.be nil
+      @stateful.change_state! :start
+      @stateful.current_state.should.equal :start
     end
 
     specify "can change states" do
-      @stateful.state = :next
-      @stateful.state.should.equal :next
+      @stateful.change_state! :next
+      @stateful.current_state.should.equal :next
     end
 
     specify "errors out on invalid state choice" do
       should.raise InvalidStateError do
-        @stateful.state = :fail_state
+        @stateful.change_state! :fail_state
       end
 
-      @stateful.state.should.not.equal :fail_state
+      @stateful.current_state.should.not.equal :fail_state
     end
 
     specify "can have a start state" do
       Stateful.starting_state :start
-      Stateful.new.state.should.equal :start
-    end
-
-    specify "can rename the state accessor" do
-      Stateful.state_accessor :my_state
-      s = Stateful.new
-      s.my_state.should.equal :start
+      Stateful.new.current_state.should.equal :start
     end
 
   end
@@ -95,24 +103,24 @@ context "SolidState" do
   context "Per-state functionality" do
 
     specify "properly uses methods defined in the current state" do
-      @stateful.state = :start
+      @stateful.change_state! :start
       @stateful.add(2, 4).should.equal 6
 
-      @stateful.state = :next
+      @stateful.change_state! :next
       @stateful.add(2, 4).should.equal -2
 
-      @stateful.state = :last
+      @stateful.change_state! :last
       @stateful.add(2, 4).should.equal 8
     end
 
     specify "states have access to methods defined outside of states" do
-      @stateful.state = :start
+      @stateful.change_state! :start
       @stateful.use_helper.should.equal 14
       @stateful.helper.should.equal 14
     end
 
     specify "state's don't have to have the same methods defined across" do
-      @stateful.state = :next
+      @stateful.change_state! :next
       should.raise NoMethodError do
         @stateful.use_helper
       end
@@ -120,8 +128,22 @@ context "SolidState" do
     end
 
     specify "WARNING: helper methods named the same as state methods use helper methods" do
-      @stateful.state = :next
+      @stateful.change_state! :next
       @stateful.outer.should.equal 10
+    end
+
+  end
+
+  context "Subclassing" do
+
+    specify "subclasses can use parent class states" do
+      state = SubState.new
+      state.current_state.should.equal :another
+      state.add(5, 2).should.equal 1
+
+      should.not.raise InvalidStateError do
+        state.change_state! :next
+      end
     end
 
   end
